08dee3c50e9a4a131bc1310409269b6c589c3337
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-21 13:41:50 +0530

    Authentication

--- app/(auth)/sign-in/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function SignIn() {
-  return <div>sign-in</div>;
-}
--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,8 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <div>Home</div>;
+  return <div>
+    {/* <UserButton /> */}
+    Home
+  </div>;
 }
