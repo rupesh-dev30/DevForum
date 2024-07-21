e0040be1cf27e0f8e897484adcbc7e4d55baf76d
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-21 13:18:08 +0530

    routing

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center">
+      {children}
+    </main>
+  );
+}
---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,3 @@
+export default function SignIn() {
+  return <div>sign-in</div>;
+}
