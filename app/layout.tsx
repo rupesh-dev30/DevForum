f0235d81534e681c28cbea34f5684c655b61b118
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-22 19:49:32 +0530

    font, assets, metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,46 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import './globals.css'
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
+import "./globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DevForum",
+  description:
+    "DevHub is the ultimate online forum for developers of all levels to connect, collaborate, and grow. Whether you're a seasoned professional or just starting out, DevHub provides a supportive and engaging environment where you can share knowledge, seek advice, and find inspiration.",
+  icons: {
+    icon: "/assets/images/site-logo.svg"
+  }
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      elements: {
+        formButtonPrimary: "primary-gradient",
+        footerActionLink: "primary-text-gradient hover:text-primary-500"
+      }
+    }}>
       <html lang="en">
-        <body>
-        <UserButton />
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           {children}
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
