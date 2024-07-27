9fa71c9655ada7f609e1fbce2c46ff093cb12d03
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-27 20:11:12 +0530

    refactor question model

--- database/question.model.ts
+++ database/question.model.ts
@@ -1,4 +1,4 @@
-import { model, models, Schema } from "mongoose";
+import { Schema, models, model, Document } from "mongoose";
 
 export interface IQuestion extends Document {
   title: string;
@@ -13,52 +13,15 @@ export interface IQuestion extends Document {
 }
 
 const QuestionSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  tags: {
-    type: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Tag",
-      },
-    ],
-  },
-  views: {
-    type: Number,
-    default: 0,
-  },
-  upvotes: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  downvotes: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  answers: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  tag: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+  views: { type: Number, default: 0 },
+  upvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  downvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  answers: [{ type: Schema.Types.ObjectId, ref: "Answer" }],
+  createdAt: { type: Date, default: Date.now },
 });
 
 const Question = models.Question || model("Question", QuestionSchema);
