386c4432bd9a7bda11332e13b4d42fcb1e64a046
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-27 20:37:16 +0530

    change mongoose.ts

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,24 +1,17 @@
 import mongoose from "mongoose";
 
-let isConnected: boolean = false;
-
 export const connectToDatabase = async () => {
   mongoose.set("strictQuery", true);
 
-  if (!process.env.MONGODB_URL) return console.log("Missing MongoDB URL");
-
-  if (isConnected) {
-    return console.log("MongoDB is already connected!");
+  if (!process.env.MONGODB_URL) {
+    return console.log("MISSING MONGODB_URL");
   }
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
       dbName: "devforum",
     });
-
-    isConnected = true;
-    console.log('MongoDB is connected');
   } catch (error) {
-    console.log("MongoDB Connection failed!", error);
+    console.log(error);
   }
 };
