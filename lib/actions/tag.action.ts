25a1de57fbf304e5e8653dad7348015657cc3903
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-28 22:37:31 +0530

    tags (minor bug , no. of question)

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,10 +1,11 @@
-"use server"
+"use server";
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tag.model";
 
-export async function getTopInteractedTags(params: GetTopInteractedTagsParams){
+export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
     connectToDatabase();
 
@@ -12,15 +13,31 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams){
 
     const user = await User.findById(userId);
 
-    if(!user) {
+    if (!user) {
       throw new Error("User not found!");
     }
 
     //Find interactions for the user and group by tags...
     // Interaction...
 
-    return [{_id: "1", name: "tag1"}, {_id: "2", name: "tag2"}, {_id: "3", name: "tag3"}]
+    return [
+      { _id: "1", name: "tag1" },
+      { _id: "2", name: "tag2" },
+      { _id: "3", name: "tag3" },
+    ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
 
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({});
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
