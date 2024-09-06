ef91b96da51153cd804d3b4ecc7e8c40722bee8b
rupesh-dev30 <cgcrupesh@gmail.com>
2024-09-07 00:52:42 +0530

    fix pagination

--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -1,52 +1,56 @@
-"use client";
-import { formUrlQuery } from "@/lib/utils";
-import { Button } from "../ui/button";
+"use client"
 
-import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery } from '@/lib/utils';
+import { Button } from '../ui/button'
+import { useRouter, useSearchParams } from 'next/navigation';
 
 interface Props {
   pageNumber: number;
   isNext: boolean;
 }
 
-export default function Pagination({ pageNumber, isNext }: Props) {
+const Pagination = ({ pageNumber, isNext }: Props) => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  function handleNavigation(direction: string) {
-    const nextPageNumber =
-      direction === "prev" ? pageNumber - 1 : pageNumber + 1;
+  const handleNavigation = (direction: string) => {
+    const nextPageNumber = direction === 'prev'
+      ? pageNumber - 1
+      : pageNumber + 1;
 
     const newUrl = formUrlQuery({
       params: searchParams.toString(),
-      key: "page",
+      key: 'page',
       value: nextPageNumber.toString(),
-    });
+    })
 
-    router.push(newUrl);
-
-    if(!isNext && pageNumber === 1) return null;
+    router.push(newUrl)
   }
+
+  if(!isNext && pageNumber === 1) return null;
 
   return (
     <div className="flex w-full items-center justify-center gap-2">
       <Button
         disabled={pageNumber === 1}
-        onClick={() => handleNavigation("prev")}
-        className="light-border-2 btn border flex min-h-[36px] items-center justify-center gap-2"
+        onClick={() => handleNavigation('prev')}
+        className="light-border-2 btn flex min-h-[36px] items-center justify-center gap-2 border"
       >
         <p className="body-medium text-dark200_light800">Prev</p>
       </Button>
-      <div className="bg-primary-500 flex justify-center items-center rounded-md px-3.5 py-2">
-        <p className="body-semibold text-light-900">{pageNumber}</p>
+      <div className="flex items-center justify-center rounded-md bg-primary-500 px-3.5 py-2">
+        <p className="body-semibold text-light-900">{pageNumber}
+        </p>
       </div>
       <Button
         disabled={!isNext}
-        onClick={() => handleNavigation("next")}
-        className="light-border-2 btn border flex min-h-[36px] items-center justify-center gap-2"
+        onClick={() => handleNavigation('next')}
+        className="light-border-2 btn flex min-h-[36px] items-center justify-center gap-2 border"
       >
         <p className="body-medium text-dark200_light800">Next</p>
       </Button>
     </div>
-  );
+  )
 }
+
+export default Pagination
